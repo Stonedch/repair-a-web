e4e79d1a5b3ab3503154bc6762cfe06e5eea4d9e
Max Mukhin <stch.max.muhin@gmail.com>
2018-09-23 13:49:14 +0300

    Add thymeleaf to index page

--- src/main/java/ru/repaira/controller/IndexController.java
+++ src/main/java/ru/repaira/controller/IndexController.java
@@ -1,21 +1,18 @@
 package ru.repaira.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String getDefault() {
-        return getHome();
+        return getIndex();
     }
 
     @GetMapping("/index")
-    public String getHome() {
+    public String getIndex() {
         return "/index";
     }
 }
