d255d77362b45dff7fa9f432d1ede20b05684062
Max Mukhin <stch.max.muhin@gmail.com>
2018-09-23 14:06:41 +0300

    Add portfolio controller and portfolio thymeleaf template

---
+++ src/main/java/ru/repaira/controller/PortfolioController.java
@@ -0,0 +1,13 @@
+package ru.repaira.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PortfolioController {
+
+    @GetMapping("/portfolio")
+    public String getPortfolio() {
+        return "/portfolio";
+    }
+}
