03611eb5f8f352bd98e6617679dd3173728f0935
Max Mukhin <stch.max.muhin@gmail.com>
2018-09-23 14:07:12 +0300

    Add price-list controller and price-list thymeleaf template

---
+++ src/main/java/ru/repaira/controller/PriceListController.java
@@ -0,0 +1,13 @@
+package ru.repaira.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PriceListController {
+
+    @GetMapping("/price-list")
+    public String getPortfolio() {
+        return "/price-list";
+    }
+}
