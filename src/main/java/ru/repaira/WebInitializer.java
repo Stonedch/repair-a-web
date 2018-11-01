27d35fc62bad337360d8bffe05e8cdf88ace898e
Max Mukhin <stch.max.muhin@gmail.com>
2018-11-01 07:09:42 +0300

    feat: add web initializer for deployment on server

---
+++ src/main/java/ru/repaira/WebInitializer.java
@@ -0,0 +1,12 @@
+package ru.repaira;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
